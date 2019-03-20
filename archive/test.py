72eae2f686c1824ac2a5847b02b77f801570d2a6
David Ferris <david.j.ferris.21@gmail.com>
2019-03-20 17:26:57 -0400

    Deleting entire archive.

--- archive/test.py
+++
@@ -1,9 +0,0 @@
-from engine import Engine, RandomEngine
-import tree
-import chess
-
-def test_game_tree():
-	e = Engine()
-	e.generate_legal_moves_and_update_game_tree(depth=2)
-	game_tree = e.get_game_tree()
-	print(game_tree.successors)
