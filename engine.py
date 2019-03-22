76d318ca0dee6d101c8a584988ca317d9208af76
David Ferris <david.j.ferris.21@gmail.com>
2019-03-22 15:25:02 -0400

    Minimax eng.

--- engine.py
+++ engine.py
@@ -1,13 +1,20 @@
+import utils
+
 class ChessEngine(object):
     def __init__(self):
         pass
     
-    def play(self, board):
+    def play(self, chess_tree):
         raise Exception("play() function not implemented.")
 
 class MiniMaxEngine(ChessEngine):
     def __init__(self):
         super()
     
-    def play(self, board):
-        pass
+    def play(self, chess_tree):
+        value, state = utils.minimax(chess_tree, 2)
+        return state
+
+
+# More engines below, ex. RLEngine, GeneticEngine, etc.
+# ...
