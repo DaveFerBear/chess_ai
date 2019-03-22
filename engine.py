c8d1a49fa44f3fe19801d8c06578d4727b7bb11b
David Ferris <david.j.ferris.21@gmail.com>
2019-03-22 15:28:57 -0400

    random engine

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import utils
+import random
 
 class ChessEngine(object):
     def __init__(self):
@@ -7,6 +8,19 @@ class ChessEngine(object):
     def play(self, chess_tree):
         raise Exception("play() function not implemented.")
 
+'''
+Example engine giving random moves.
+'''
+class RandomEngine(ChessEngine):
+    def __init__(self):
+        super()
+    
+    def play(self, chess_tree):
+        return random.choice(chess_tree.leaf_boards)
+
+'''
+Engine using exclusively minimax with alpha-beta pruning.
+'''
 class MiniMaxEngine(ChessEngine):
     def __init__(self):
         super()
