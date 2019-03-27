8f198b39419e8caa96fc33edd022b48c5833986f
David Ferris <david.j.ferris.21@gmail.com>
2019-03-27 19:54:53 -0400

    Working game playing itselfgit add -A

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ def play_engines(e1, e2, print_out=False):
     while not b.is_game_over():
         cur_player = e1 if white_to_play else e2
         move = cur_player.play(ct)
-        ct = ct.find_new_head(move)
-        white_to_play = not white_to_play
+
+        print("Selected Move: {}".format(move))
+
+        ct = ct.find_new_head(move) # Travel down chess tree
+        ct.generate_leaf_nodes()
+        white_to_play = not white_to_play # Change player
 
         print(ct.board)
 
 
 if __name__ == '__main__':
 
-    e1 = engine.RandomEngine()
+    e1 = engine.GeneticEngine()
     e2 = engine.GeneticEngine()
 
     play_engines(e1, e2, print_out=True)
